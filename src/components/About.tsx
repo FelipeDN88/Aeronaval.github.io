b71dbc277db41a045c271bdb330796420fbad9eb
Luiz Arlochi <luiz.arlochi@gmail.com>
2024-11-21 13:21:19 -0300

    fix about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,10 +12,7 @@ const About: React.FC = () => {
             Bem-vindo à Aeronaval
           </h1>
           <p className={`${TextStyles.paragraph} ${AboutStyles.justifiedText}`}>
-          A Aeronaval é uma empresa dedicada ao desenvolvimento de soluções tecnológicas para os setores aeroespacial e naval. Nossa missão é expandir o horizonte de possibilidades para nossos clientes, oferecendo produtos e serviços que agreguem valor.
-         <br />
-         <br />
-          Fundada por ex-integrante do Exército Brasileiro, a Aeronaval começou sua trajetória com o desenvolvimento de hovercrafts, projetados para transporte anfíbio de passageiros na desafiadora região amazônica. Atendendo os mercados civil e militar, nossos projetos são concebidos para oferecer soluções robustas, versáteis e econômicas, garantindo confiabilidade, disponibilidade, manutenibilidade e segurança durante todo o ciclo de vida dos produtos.
+          A Aeronaval é uma empresa dedicada ao desenvolvimento de soluções tecnológicas para os setores aeroespacial e naval. Nossa missão é expandir o horizonte de possibilidades para nossos clientes, oferecendo produtos e serviços inovadores.
          <br />
          <br />
          Convidamos você a explorar as possibilidades que a Aeronaval tem a oferecer. Descubra como nossa expertise e inovação podem transformar suas ideias em realidade e impulsionar o sucesso de suas operações.
